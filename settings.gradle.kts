d666297c40667012294ce2651ebe297087602a81
alen-heo <58537670+hubtwork@users.noreply.github.com>
2025-08-16 16:38:28 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "loopers-java-spring-template"
+
+include(
+    ":apps:commerce-api",
+    ":modules:jpa",
+    ":modules:redis",
+    ":supports:jackson",
+    ":supports:logging",
+    ":supports:monitoring",
+)
+
+// configurations
+pluginManagement {
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+
+    repositories {
+        maven { url = uri("https://repo.spring.io/milestone") }
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+            }
+        }
+    }
+}
